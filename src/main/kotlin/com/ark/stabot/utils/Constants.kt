977b97245ab55b317c16001ff82788a77594dab5
amanverma-765 <akverma4aman@gmail.com>
2025-04-06 07:41:51 +0530

    update constants with latest requirements

--- src/main/kotlin/com/ark/stabot/utils/Constants.kt
+++ src/main/kotlin/com/ark/stabot/utils/Constants.kt
@@ -4,9 +4,9 @@ object Constants {
     const val CAPTCHA_URL = "https://tmrsearch.ipindia.gov.in/eregister/captcha.ashx"
     const val TRADEMARK_URL = "https://tmrsearch.ipindia.gov.in/eregister/Application_View.aspx"
     const val GET_CAPTCHA_URL = "https://tmrsearch.ipindia.gov.in/eregister/Viewdetails_Copyright.aspx/GetCaptcha"
-    const val MAX_THREADS = 50 // number of threads to run scraper on
+    const val MAX_THREADS = 25 // number of threads to run scraper on
     const val INITIAL_TRADEMARK = "478006" // starting trademark
-    const val TRADEMARK_CHUNK_SIZE = 10000 // trademarks to scrape on each run
-    const val NA_THRESHOLD = 100 // Trademarks to be null to stop the scraper
-    const val TASK_FREQ = 600000L // Task frequency (Every 15 Mins)
+    const val TRADEMARK_CHUNK_SIZE = 5000 // trademarks to scrape on each run
+    const val NA_THRESHOLD = 2500 // Trademarks to be null to stop the scraper
+    const val TASK_FREQ = 600000L // Task frequency (Every 10 Mins)
 }
